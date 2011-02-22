72f68ea6116b088d3bf32bd7b9b7a685f2a4ea5c
Silence <silence@immortal-forces.net>
2011-02-22 21:48:49 +0100

    Removed old constructor

--- src/net/immortal_forces/silence/plugin/simplegod/SimpleGod.java
+++ src/net/immortal_forces/silence/plugin/simplegod/SimpleGod.java
@@ -21,21 +21,18 @@
 
 package net.immortal_forces.silence.plugin.simplegod;
 
-import java.io.File;
 import java.util.Timer;
 import org.bukkit.entity.Player;
-import org.bukkit.Server;
 import org.bukkit.plugin.PluginDescriptionFile;
-import org.bukkit.plugin.PluginLoader;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SimpleGod extends JavaPlugin
 { 
   private Timer m_Timer = new Timer(true);
   
-  public SimpleGod(PluginLoader pluginLoader, Server instance, PluginDescriptionFile desc, File folder, File plugin, ClassLoader cLoader)
+  public SimpleGod()
   {
-    super(pluginLoader, instance, desc, folder, plugin, cLoader);
+    super();
   }
 
   public void onEnable() 
